a1c512fa03aad61367479dfa01ddef51902d3ff7
sharooq15 <sharooqannauniv.ceg@gmail.com>
2020-11-28 13:30:21 +0530

    Add test case for creating user

---
+++ src/__tests__/user.test.ts
@@ -0,0 +1,21 @@
+/*
+All the test that are related to user will be done here. 
+*/
+
+import {
+  signup,
+} from '../../src/server/controllers'
+
+describe('Testing User Related Operations', () => {
+  it('Signup', async () => {
+    const req = {
+      body: {
+        username: 'sharooq',
+        password: 'dummypassword'
+      }
+    }
+    const result = await signup(req, null);
+    console.log('result', result);
+    expect(result).toBe(true);
+  });
+});
