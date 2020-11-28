4d2de5b506788c3903400374bb26760d7d6b7327
sharooq15 <sharooqannauniv.ceg@gmail.com>
2020-11-28 10:10:28 +0530

    Make sure the basic implementation for user is working

--- src/server/middlewares/index.ts
+++ src/server/middlewares/index.ts
@@ -1,30 +1,30 @@
 import bodyParser from 'body-parser';
 
 export function redirect (req: any, res: any) {
-    res.redirect('/');
+  res.redirect('/');
 }
 
 export function rawBodyParser (req: any, res: any, next: any) {
-    bodyParser.raw({
-        limit: '50mb'
-    })(req, res, next);
+  bodyParser.raw({
+    limit: '50mb'
+  })(req, res, next);
 }
 
 export function jsonBodyParser (req: any, res: any, next: any) {
-    bodyParser.json()(req, res, next);
+  bodyParser.json()(req, res, next);
 }
 
 export function encodedBodyParser (req: any, res: any, next: any) {
-    bodyParser.urlencoded({
-        extended: true,
-        limit: '50mb'
-    })(req, res, next);
+  bodyParser.urlencoded({
+    extended: true,
+    limit: '50mb'
+  })(req, res, next);
 }
 
 export function errorHandler (err: any, req: any, res: any, next: any) {
-    res.status(500).json({
-        status: err.response.status,
-        statusText: err.response.data.Message,
-        details: err.response.data.MessageDetail
-    });
+  res.status(500).json({
+    status: err.response.status,
+    statusText: err.response.data.Message,
+    details: err.response.data.MessageDetail
+  });
 }
