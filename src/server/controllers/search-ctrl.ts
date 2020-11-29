6097ce93b5009785a5c9f2a9c14de4f1d3716ea1
sharooq15 <sharooqannauniv.ceg@gmail.com>
2020-11-29 12:50:15 +0530

    Fix search issues

--- src/server/controllers/search-ctrl.ts
+++ src/server/controllers/search-ctrl.ts
@@ -1,77 +1,85 @@
-import { ItemList } from 'aws-sdk/clients/personalizeruntime'
-import { docClient } from '../../api-utils'
-import { tableNames } from '../../common'
-import type { Question } from './question-ctrl'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { ItemList } from 'aws-sdk/clients/personalizeruntime';
+import { docClient } from '../../api-utils';
+import { tableNames } from '../../common';
+import type { Question } from './question-ctrl';
 
 type SearchInputRequest = {
   body: {
-    input: string
-  }
-}
+    input: string;
+  };
+};
 
 type SearchDocument = {
-  obj: Question
-  searchElement: string[]
-}
+  obj: Question;
+  searchElement: string[];
+};
 
 const constructSearchDocuments = (input?: ItemList): SearchDocument[] => {
   if (input) {
     const searchDocuments = input.map((i) => {
       const searchDocument: SearchDocument = {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         obj: {
+          // @ts-ignore
           id: i.id,
+          // @ts-ignore
           stem: i.t,
+          // @ts-ignore
           description: i.d,
+          // @ts-ignore
           votes: i.vt,
+          // @ts-ignore
           comments: i.c,
+          // @ts-ignore
           tags: i.tgs,
         },
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         searchElement: [i.t, i.d],
-      }
-      return searchDocument
-    })
-    return searchDocuments
+      };
+      return searchDocument;
+    });
+    return searchDocuments;
   } else {
-    return []
+    return [];
   }
-}
+};
+
+// Note: This is not a optimal solutio for search, we can achieve this using cloud search or elastic search
 
 const search = async (req: SearchInputRequest, res: any): Promise<boolean> => {
   const {
     body: { input },
-  } = req
-  const questions: Question[] = []
+  } = req;
+  const questions: Question[] = [];
   try {
     const params = {
       TableName: tableNames.QUESTION,
-    }
-    const responseItems = await docClient.scan(params).promise()
-    const searchDocuments = constructSearchDocuments(responseItems.Items)
-    console.log('Sd', searchDocuments)
+    };
+    const responseItems = await docClient.scan(params).promise();
+    const searchDocuments = constructSearchDocuments(responseItems.Items);
     if (searchDocuments && searchDocuments.length) {
       searchDocuments.forEach((doc) => {
-        if (doc.searchElement.includes(input)) {
-          questions.push(doc.obj)
+        if (
+          doc.searchElement[0].includes(input) ||
+          doc.searchElement[1].includes(input)
+        ) {
+          questions.push(doc.obj);
         }
-      })
+      });
     }
-    console.log('qusetions', questions)
     if (res && questions && questions.length) {
-      res.send(questions)
+      res.send(questions);
     } else if (res) {
-      res.send('Please Pass an appropriate key word')
+      res.send('Please Pass an appropriate key word');
     }
   } catch (e) {
-    res.send('Error Searching the record')
-    console.log('Error Searching the record', e)
-    return false
+    res.send('Error Searching the record');
+    console.log('Error Searching the record', e);
+    return false;
   }
 
-  return true
-}
+  return true;
+};
 
-export { search }
+export { search };
